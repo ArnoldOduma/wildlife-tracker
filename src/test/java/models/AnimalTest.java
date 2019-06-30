0c792c26e3e65f57b9f18387e51dda00938faea9
Arnold Oduma <arnoldcliff98@gmail.com>
2019-06-30 23:32:32 +0300

    This commit will:  1-> Test for the working of setters and getters in the animals class.  2-> Test if the method for saving an animal to the DB works

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,58 +1,60 @@
 package models;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class AnimalTest {
 
-    public Animal setUpAnimal(){return new Animal("Rhino",5);}
 
     @Test
-    public void getAge() {
-        Animal newAnimal = setUpAnimal();
-        assertEquals(5,newAnimal.getAge());
+    public void getAnimal_age() {
+        Animal newAnimal = Animal.setUpNewAnimal();
+        assertEquals("Old",newAnimal.getAnimal_age());
     }
 
     @Test
-    public void setAge() {
-        Animal newAnimal = setUpAnimal();
-        newAnimal.setAge(10);
-        assertEquals(10,newAnimal.getAge());
+    public void setanimal_age() {
+        Animal newAnimal = Animal.setUpNewAnimal();
+        newAnimal.setAnimal_age("Young");
+        assertEquals("Young",newAnimal.getAnimal_age());
     }
 
-    @Test
-    public void getId() {
-        Animal.clearAll();
-        Animal newAnimal = setUpAnimal();
-        assertEquals(1,newAnimal.getId());
-    }
 
     @Test
     public void setId() {
-        Animal newAnimal = setUpAnimal();
+        Animal newAnimal = Animal.setUpNewAnimal();
         newAnimal.setId(3);
         assertEquals(3,newAnimal.getId());
     }
 
     @Test
-    public void getAllAnimals() {
-        Animal.clearAll();
-        Animal newAnimal = setUpAnimal();
-        Animal anotherAnimal = setUpAnimal();
-        assertEquals(2,Animal.getAllAnimals().size());
-    }
-
-    @Test
     public void getName() {
-        Animal newAnimal = setUpAnimal();
-        assertEquals("Rhino",newAnimal.getName());
+        Animal newAnimal = Animal.setUpNewAnimal();
+        assertEquals("Cat",newAnimal.getAnimal_name());
     }
 
     @Test
     public void setName() {
-        Animal newAnimal = setUpAnimal();
-        newAnimal.setName("Hyena");
-        assertEquals("Hyena",newAnimal.getName());
+        Animal newAnimal = Animal.setUpNewAnimal();
+        newAnimal.setAnimal_name("Hyena");
+        assertEquals("Hyena",newAnimal.getAnimal_name());
+    }
+
+    @Test
+    public void equals1() {
+    }
+
+    @Test
+    public void hashCode1() {
+    }
+
+    @Test
+    public void saveAnimal() {
+        Animal newAnimal = Animal.setUpNewAnimal();
+        int initialId = newAnimal.getId();
+        newAnimal.saveAnimal(newAnimal);
+        assertNotEquals(initialId,newAnimal.getId());
     }
 }
