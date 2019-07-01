f6af50b934a26921ed65043db1bc69cfc16c54a5
Arnold Oduma <arnoldcliff98@gmail.com>
2019-07-01 05:55:47 +0300

    This commit will:  1-> Create methods to get enf=dangered animals by id and return the object.

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -7,16 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class EndangeredAnimal implements AnimalInterface {
+public class EndangeredAnimal  {
     private int animal_id;
     private String animal_name;
     private String animal_health;
     private String animal_age;
     private int id;
 
-    public EndangeredAnimal(int animal_id, String animal_name,String animal_health, String animal_age ){
+    public EndangeredAnimal(String animal_name,String animal_health, String animal_age ){
         this.animal_name = animal_name;
-        this.animal_id = animal_id;
         this.animal_health = animal_health;
         this.animal_age = animal_age;
         this.id = 1;
@@ -79,12 +78,11 @@ public class EndangeredAnimal implements AnimalInterface {
         return Objects.hash(getAnimal_id(), getAnimal_name(), getAnimal_health(), getAnimal_age(), getId());
     }
 
-    @Override
-    public void saveAnimal(Animal animal) {
+//    @Override
+    public void saveAnimal(EndangeredAnimal endangeredAnimal) {
         try (Connection conn = Database.sql2otest2.open()){
-            String sql = "INSERT INTO  animals(animal_id, animal_name, animal_age, animal_health ) VALUES (:animal_id, :animal_name, :animal_age,:animal_health);";
+            String sql = "INSERT INTO  endangered_animals(animal_name, animal_age, animal_health ) VALUES (:animal_name, :animal_age,:animal_health);";
             this.id = (int) conn.createQuery(sql, true)
-                    .addParameter("animal_id", this.animal_id)
                     .addParameter("animal_name", this.animal_name)
                     .addParameter("animal_age", this.animal_age)
                     .addParameter("animal_health", this.animal_health)
@@ -93,16 +91,26 @@ public class EndangeredAnimal implements AnimalInterface {
         }
     }
 
-    @Override
-    public List<Animal> getAll() {return null;}
-
-    @Override
-    public Animal findById(int id) {
-        String sql = "SELECT * FROM animals WHERE id=:id";
-        try (Connection conn = Database.sql2otest2.open()){
+    public static List<EndangeredAnimal> getAllAnimals() {
+        try(Connection conn = Database.sql2otest2.open()){
+            String sql = "SELECT * FROM endangered_animals ORDER BY id DESC;";
             return conn.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(EndangeredAnimal.class);
+        }
+    }
+
+//    @Override
+    public EndangeredAnimal findById(int id) {
+        String sql = "SELECT * FROM endangered_animals WHERE id=:id";
+        try (Connection conn = Database.sql2otest2.open()){
+            EndangeredAnimal animal = conn.createQuery(sql)
                     .addParameter("id",id)
-                    .executeAndFetchFirst(Animal.class);
+                    .executeAndFetchFirst(EndangeredAnimal.class);
+             return animal;
+        }catch (IndexOutOfBoundsException ex){
+            System.out.println(ex);
+            return null;
         }
     }
 
